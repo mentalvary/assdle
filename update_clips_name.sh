56da12033c39c27522195aa4751a40c0afc76cae
mentalvary <6898650+mentalvary@users.noreply.github.com>
2026-08-20 23:34:31 +0200

    fixes

--- update_clips_name.sh
+++ update_clips_name.sh
@@ -1,3 +1,7 @@
+if [ -f clips.js ]; then
+    rm clips-*.js
+fi
+
 new_name="clips-$(sha256sum clips*.js | awk '{print $1}').js"
 mv clips*.js $new_name
 sed -e 's/src="clips.*.js"/src="'$new_name'"/' -i index.html
