a231741f60e8d582c089dcd357b299a2497d9572
mentalvary <6898650+mentalvary@users.noreply.github.com>
2026-09-06 00:05:03 +0200

    update clips

--- src/dailies.js
+++ src/dailies.js
@@ -1,4 +1,201 @@
 const fixedDailies = {
+    "2026-09-06": [
+        {
+            "mainClip": {
+                "vid": "s7Xy1ZxRlDg",
+                "time": 11159,
+                "duration": 3,
+                "text": "and that's myega ass of",
+                "index": 163,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "sn0eA350-nc",
+                    "time": 21634,
+                    "duration": 1,
+                    "text": "to be something ass and then I loot ",
+                    "index": 5801,
+                    "votes": 0
+                },
+                {
+                    "vid": "E-LcSZKCr4M",
+                    "time": 19605,
+                    "duration": 26,
+                    "text": "clearly getting my ass kicked. All",
+                    "index": 840,
+                    "votes": 0
+                },
+                {
+                    "vid": "s7Xy1ZxRlDg",
+                    "time": 11159,
+                    "duration": 3,
+                    "text": "and that's myega ass of",
+                    "index": 163,
+                    "isMain": true,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "AsFaHxtVBIo",
+                "time": 9221,
+                "duration": 8,
+                "text": "get in your ass cheeks when you watch ",
+                "index": 5984,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "quYui5vDTg0",
+                    "time": 1481,
+                    "duration": 12,
+                    "text": "tnps kind of ass but",
+                    "index": 7285,
+                    "votes": 0
+                },
+                {
+                    "vid": "AsFaHxtVBIo",
+                    "time": 9221,
+                    "duration": 6,
+                    "text": "get in your ass cheeks when you watch ",
+                    "index": 5984,
+                    "isMain": true,
+                    "votes": 0
+                },
+                {
+                    "vid": "CvxcsYcBdoQ",
+                    "time": 18077,
+                    "duration": 6,
+                    "text": "Jesus was so ass to",
+                    "index": 6176,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "iv_aNfojTp0",
+                "time": 11422,
+                "duration": 6,
+                "text": "I worked my ass off to get this ",
+                "index": 4973,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "eL9-nYNyqU4",
+                    "time": 11129,
+                    "duration": 4,
+                    "text": "Yeah. Nasty ass weapon that one.",
+                    "index": 2818,
+                    "votes": 0
+                },
+                {
+                    "vid": "39LD4c_Mi2o",
+                    "time": 439,
+                    "duration": 6,
+                    "text": "it's a foggy ass. Holy foggy ass, ",
+                    "index": 1187,
+                    "votes": 0
+                },
+                {
+                    "vid": "iv_aNfojTp0",
+                    "time": 11422,
+                    "duration": 6,
+                    "text": "I worked my ass off to get this ",
+                    "index": 4973,
+                    "isMain": true,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "QX-QX6KLtG8",
+                "time": 6211,
+                "duration": 3,
+                "text": "and eats my ass.",
+                "index": 1059,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "QX-QX6KLtG8",
+                    "time": 6211,
+                    "duration": 3,
+                    "text": "and eats my ass.",
+                    "index": 1059,
+                    "isMain": true,
+                    "votes": 0
+                },
+                {
+                    "vid": "Tq8eFGhMeqg",
+                    "time": 9432,
+                    "duration": 18,
+                    "text": "is a wild ass name.",
+                    "index": 7419,
+                    "votes": 0
+                },
+                {
+                    "vid": "bQk0X7DGX4c",
+                    "time": 19872,
+                    "duration": 22,
+                    "text": "It's a scythe ass. What do you ",
+                    "index": 7468,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "lX7jxDtEZZc",
+                "time": 24231,
+                "duration": 2,
+                "text": "crawled up my ass and made me pass ",
+                "index": 4662,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "cR6NxKbku4s",
+                    "time": 19080,
+                    "duration": 3,
+                    "text": "with this stupid ass Invisalign so I can ",
+                    "index": 9436,
+                    "votes": 0
+                },
+                {
+                    "vid": "5hBli5h57UM",
+                    "time": 4453,
+                    "duration": 6,
+                    "text": "seen some freaky ass characters",
+                    "index": 8922,
+                    "votes": 0
+                },
+                {
+                    "vid": "lX7jxDtEZZc",
+                    "time": 24231,
+                    "duration": 2,
+                    "text": "crawled up my ass and made me pass ",
+                    "index": 4662,
+                    "isMain": true,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        }
+    ],
     "2026-09-05": [
         {
             "mainClip": {
