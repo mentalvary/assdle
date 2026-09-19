68ca371061be696153202b2183a05abc2844f23c
mentalvary <6898650+mentalvary@users.noreply.github.com>
2026-09-19 12:56:50 +0200

    update clips

--- src/dailies.js
+++ src/dailies.js
@@ -1,4 +1,201 @@
 const fixedDailies = {
+    "2026-09-19": [
+        {
+            "mainClip": {
+                "vid": "nARbkIR2xGU",
+                "time": 5764,
+                "duration": 4,
+                "text": "why in my ass P wait she's dead ",
+                "index": 7153,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "bsrsenMB7P8",
+                    "time": 11142,
+                    "duration": 6,
+                    "text": "is a freaky ass enemy",
+                    "index": 5026,
+                    "votes": 0
+                },
+                {
+                    "vid": "Nbc7MgExt_U",
+                    "time": 9995,
+                    "duration": 3,
+                    "text": "Where's that fishy ass link? What is that ",
+                    "index": 2591,
+                    "votes": 0
+                },
+                {
+                    "vid": "nARbkIR2xGU",
+                    "time": 5764,
+                    "duration": 4,
+                    "text": "why in my ass P wait she's dead ",
+                    "index": 7153,
+                    "isMain": true,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "Ol0V1QKtryc",
+                "time": 30770,
+                "duration": 7,
+                "text": "a lot of ass and ads. I mean, ",
+                "index": 1851,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "Ol0V1QKtryc",
+                    "time": 30770,
+                    "duration": 7,
+                    "text": "a lot of ass and ads. I mean, ",
+                    "index": 1851,
+                    "isMain": true,
+                    "votes": 0
+                },
+                {
+                    "vid": "qkWkSA3uNik",
+                    "time": 2767,
+                    "duration": 1,
+                    "text": "M on my ass I was I need ",
+                    "index": 5431,
+                    "votes": 0
+                },
+                {
+                    "vid": "H9rvWZhqRJM",
+                    "time": 19069,
+                    "duration": 5,
+                    "text": "Particles in my ass. Like",
+                    "index": 1833,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "ndK6T2F3cYA",
+                "time": 19678,
+                "duration": 3,
+                "text": "god, the mid ass in my ass. Okay, ",
+                "index": 6891,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "8fVGql9t7Co",
+                    "time": 1095,
+                    "duration": 9,
+                    "text": "So ass. Wait, this is ",
+                    "index": 9725,
+                    "votes": 0
+                },
+                {
+                    "vid": "ndK6T2F3cYA",
+                    "time": 19678,
+                    "duration": 3,
+                    "text": "god, the mid ass in my ass. Okay, ",
+                    "index": 6891,
+                    "isMain": true,
+                    "votes": 0
+                },
+                {
+                    "vid": "sTiZVwUuKy8",
+                    "time": 15010,
+                    "duration": 13,
+                    "text": "they pinch my ass and I'm a",
+                    "index": 5136,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "ccHFkZseDUQ",
+                "time": 14026,
+                "duration": 9,
+                "text": "not in my ass. Was not in ",
+                "index": 2566,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "ccHFkZseDUQ",
+                    "time": 14026,
+                    "duration": 9,
+                    "text": "not in my ass. Was not in ",
+                    "index": 2566,
+                    "isMain": true,
+                    "votes": 0
+                },
+                {
+                    "vid": "CvxcsYcBdoQ",
+                    "time": 5155,
+                    "duration": 2,
+                    "text": "take out your ass we know the",
+                    "index": 6174,
+                    "votes": 0
+                },
+                {
+                    "vid": "uNfy0jPsezA",
+                    "time": 17653,
+                    "duration": 3,
+                    "text": "boy get your ass over here little green",
+                    "index": 10834,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        },
+        {
+            "mainClip": {
+                "vid": "0i0vP-yhXws",
+                "time": 571,
+                "duration": 7,
+                "text": "mods get that ass banned. [__]",
+                "index": 8495,
+                "isMain": true,
+                "votes": 0
+            },
+            "clips": [
+                {
+                    "vid": "0i0vP-yhXws",
+                    "time": 571,
+                    "duration": 7,
+                    "text": "mods get that ass banned. [__]",
+                    "index": 8495,
+                    "isMain": true,
+                    "votes": 0
+                },
+                {
+                    "vid": "MfrmYbUzWyo",
+                    "time": 3002,
+                    "duration": 2,
+                    "text": "definitely get some ass effect when you're melling ",
+                    "index": 6632,
+                    "votes": 0
+                },
+                {
+                    "vid": "j6zdUyN5FIc",
+                    "time": 4783,
+                    "duration": 8,
+                    "text": "to the Rainy ass just be careful let's",
+                    "index": 7865,
+                    "votes": 0
+                }
+            ],
+            "result": "NOT_PLAYED"
+        }
+    ],
     "2026-09-13": [
         {
             "mainClip": {
